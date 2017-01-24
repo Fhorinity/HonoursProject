884f696d6a120f35fe67d2cb9d3107d146d0cce4
mbrooks7 <mbrooks7>
2017-01-24 18:04:31 +0000

    Basic setup.

---
+++ Honours_Project/Assets/Scripts/Gravitational Pulser/Raycasting.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycasting : MonoBehaviour {
+
+    private float range = 50f;
+    public Camera _camera;
+    public bool canSee = false;
+    public bool canCarry = false;
+    public bool isCarrying = false;
+    public GameObject _CarriedObject;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = _camera.ViewportPointToRay(new Vector3(.5f, .5f, .5f));
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
+            if (hit.collider.tag == "Throwable")
+            {
+                canSee = true;
+                Debug.Log("Hi I can be thrown");
+                CarryCheck();
+
+
+            }
+            else
+            {
+                canSee = false;
+            }
+            
+            
+
+
+        }
+    }
+
+    void CarryCheck()
+    {
+        if (Input.GetMouseButtonDown(1) && canSee == true)
+        {
+            Debug.Log("Pull to me");
+            isCarrying = true;
+            // Puesdo Intention
+            // add force to object to pull towards player
+            // Always make sure the force is directed at the player
+            //Once the position of the object is within a certain range slighlty rise the Y position value
+            // Once at height that is level with reference point
+            // Set cancarry bool to true;
+            // Attach object to camera as child to camera.
+            //Set up if statement to check it is carrying and whilst being carried
+            //if hits a object / wall it its orientation will change
+            // but as soon as no collision left it will snap back to the orientation it was picked up in.
+
+            if (isCarrying)
+            {
+                DropCheck();
+                LaunchCheck();
+            }
+            
+
+        }
+    }
+    void DropCheck()
+    {
+        if (Input.GetMouseButtonDown(1) && isCarrying == true)
+        {
+            Debug.Log("Dropped object");
+            isCarrying = false;
+            // Set up code for dropping object where it is.
+        }
+    }
+    void LaunchCheck()
+    {
+        if (Input.GetMouseButtonDown(1) && isCarrying == true)
+        {
+            Debug.Log("Launched Object");
+            isCarrying = false;
+            //Set up code for launching object.
+            //Use add force etc and fire in a forward direction to where the mouse is facing.
+        }
+    }
+        
+}
---
+++ Honours_Project/Assets/Scripts/Gravitational Pulser/ThrowObject.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour
+{
+    public Transform _player;
+    public Transform _camera;
+    public float throwForce = 10;
+    public bool hasPlayer = false; //Checks if player isi n range to pick up object
+    public bool beingCarried = false;
+    public AudioClip[] soundToPlay;
+    private AudioSource audio;
+    private bool touched = false;
+   // public Camera cam;
+    public float distance = 10f;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+      //  Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, .5f));
+
+        float dist = Vector3.Distance(gameObject.transform.position, _player.position);
+        if (dist <= 2.5f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+
+      //  if (Physics.Raycast(ray, out hit, distance))
+        //{
+
+        //}
+
+        if (hasPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = _camera;
+            beingCarried = true;
+        }
+        if (beingCarried)
+        {
+            if (touched)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(_camera.forward * throwForce);
+                RandomAudio();
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+    }
+    void RandomAudio()
+    {
+        if (audio.isPlaying)
+        {
+            return;
+        }
+        audio.clip = soundToPlay[Random.Range(0, soundToPlay.Length)];
+        audio.Play();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+
+    
+}
